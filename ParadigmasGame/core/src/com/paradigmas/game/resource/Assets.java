819fb72c62aa76d7b170cfcb9a105dc6c4f90bde
Alisson C. Schmidt <acschmidt@inf.ufsm.br>
2021-08-18 14:46:18 -0300

    Update Assets.java

--- ParadigmasGame/core/src/com/paradigmas/game/resource/Assets.java
+++ ParadigmasGame/core/src/com/paradigmas/game/resource/Assets.java
@@ -9,10 +9,10 @@ public class Assets {
     public static final AssetManager manager = new AssetManager();
 
 
-    public static final AssetDescriptor<Texture> dirt = new AssetDescriptor<Texture>("Mapa_Texturas/Dirt_texture.png", Texture.class);
-    public static final AssetDescriptor<Texture> cobblestone = new AssetDescriptor<Texture>("Mapa_Texturas/Pedra_texture.png", Texture.class);
-    public static final AssetDescriptor<Texture> obsidian = new AssetDescriptor<Texture>("Mapa_Texturas/Obsidian_Texture.png", Texture.class);
-    public static final AssetDescriptor<Texture> playerH = new AssetDescriptor<Texture>("Personagem_principal_H/Cow__000.png", Texture.class);
+    public static final AssetDescriptor<Texture> dirt = new AssetDescriptor<>("Mapa_Texturas/Dirt_texture.png", Texture.class);
+    public static final AssetDescriptor<Texture> cobblestone = new AssetDescriptor<>("Mapa_Texturas/Pedra_texture.png", Texture.class);
+    public static final AssetDescriptor<Texture> obsidian = new AssetDescriptor<>("Mapa_Texturas/Obsidian_Texture.png", Texture.class);
+    public static final AssetDescriptor<Texture> playerH = new AssetDescriptor<>("Personagem_principal_H/Cow__000.png", Texture.class);
 
     public static void load() {
         manager.load(dirt);

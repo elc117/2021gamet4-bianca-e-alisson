2f140576d2654f838cc07858429ce267c6249822
Alisson C. Schmidt <acschmidt@inf.ufsm.br>
2021-08-17 23:01:51 -0300

    Trocando por uma versão realmente sem Android.
    Na ultima alteração, a pasta errada foi upada.

---
+++ ParadigmasGame/desktop/src/com/paradigmas/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.paradigmas.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.paradigmas.game.ParadigmasGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ParadigmasGame(), config);
+	}
+}
